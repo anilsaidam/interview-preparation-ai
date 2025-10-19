b8fdc87ce6c2fb4732c10e81a49784f2d84c4d77
Saidam Anil Kumar <anilkumarakvlcss6616@gmail.com>
2025-10-19 18:03:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.use(
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
