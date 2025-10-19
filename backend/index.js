f01c1057f1d397d6fd60da78012c7b8178d643d6
Saidam Anil Kumar <anilkumarakvlcss6616@gmail.com>
2025-10-19 18:16:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.use(
   cors({
     origin: [
       "https://career-companion-ai-xi.vercel.app",
-      "http://localhost:5173",
+      "http://localhost:5174",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-type", "Authorization"],
